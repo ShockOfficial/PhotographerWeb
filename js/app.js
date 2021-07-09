567cf71180e827bc032fc58b88d2ad4d1e6a972f
Paweł Motyka <pglmotyka@gmial.com>
2021-07-09 21:50:35 +0200

    add carousel in vanilla JS

--- js/app.js
+++ js/app.js
@@ -2,12 +2,38 @@ let $menuItems;
 let $barsBox;
 let $bars;
 let $menuBox;
+const carousel = {
+	currentImgIndex: 0,
+	container: '',
+	imgArr: '',
+	nextBtn: '',
+	prevBtn: '',
+	status: '',
+	intervalId: '',
+};
 
 function prepareDomElements() {
 	$menuItems = [...document.querySelectorAll('.nav__box .nav__link')];
 	$barsBox = document.querySelector('.nav__bars-box');
 	$bars = document.querySelector('.nav__bars');
 	$menuBox = document.querySelector('.nav__box');
+
+	carousel.imgArr = [...document.querySelectorAll('.carousel__img')];
+	carousel.nextBtn = document.querySelector('.carousel__btn--left');
+	carousel.prevBtn = document.querySelector('.carousel__btn--right');
+	carousel.status = document.querySelector('.carousel__photo-status');
+	carousel.container = document.querySelector('.carousel__image-field');
+
+	carousel.nextBtn.addEventListener('click', slideImage);
+	carousel.prevBtn.addEventListener('click', slideImage);
+	carousel.imgArr.forEach((el) => {
+		el.addEventListener('contextmenu', (e) => {
+			e.preventDefault();
+		});
+	});
+	carousel.imgArr.forEach((el) => {
+		el.draggable = false;
+	});
 }
 
 function prepareDomEvnets() {
@@ -47,11 +73,50 @@ function swipeMenu() {
 	}, 100);
 }
 
+function slideImage(e) {
+	// Button implementation;
+
+	clearInterval(carousel.intervalId);
+
+	if (!Boolean(e) || e.target.id === 'right') {
+		carousel.currentImgIndex < carousel.imgArr.length - 1
+			? carousel.currentImgIndex++
+			: (carousel.currentImgIndex = 0);
+	} else {
+		carousel.currentImgIndex > 0
+			? carousel.currentImgIndex--
+			: (carousel.currentImgIndex = carousel.imgArr.length - 1);
+	}
+
+	const currentImg = carousel.imgArr[carousel.currentImgIndex];
+
+	// Display current image possition in UI
+	carousel.status.textContent = `${carousel.currentImgIndex + 1} / ${
+		carousel.imgArr.length
+	}`;
+
+	// Swipe to appropriate image
+	carousel.container.style.transform = `translateX(-${
+		currentImg.width * carousel.currentImgIndex
+	}px)`;
+
+	startAutoSwiping();
+}
+
+function startAutoSwiping() {
+	// Automaticly swipe Images
+	carousel.intervalId = setInterval(() => {
+		slideImage();
+	}, 3000);
+}
+
 const main = function () {
 	prepareDomElements();
 	prepareDomEvnets();
 	setMenuType();
+	startAutoSwiping();
 };
 
 document.addEventListener('DOMContentLoaded', main);
 window.addEventListener('resize', setMenuType);
+window.addEventListener('resize', slideImage);
